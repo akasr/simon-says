5395db73c21c73ffd8d427f67883ad35b484adc6
Akash Raj <ebookexer@gmail.com>
2025-03-13 19:27:41 +0530

    Complete basic game design and structure

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
+// $ and $$ are shortcuts for querySelector and querySelectorAll
 const $ = function () {
-  return document.querySelector.apply(this, arguments);
+  return document.querySelector.apply(document, arguments);
 };
 const $$ = function () {
-  return document.querySelectorAll.apply(this, arguments);
+  return document.querySelectorAll.apply(document, arguments);
 };
+
+// Add custom methods to HTMLElement prototype
+// These methods are used to add and remove event listeners
 HTMLElement.prototype.on = HTMLElement.prototype.addEventListener;
 HTMLElement.prototype.off = HTMLElement.prototype.removeEventListener;
+// and to select elements
 HTMLElement.prototype.$ = function (s) {
   return this.querySelector(s);
 };
 HTMLElement.prototype.$$ = function (s) {
   return this.querySelectorAll(s);
 };
+
+
+const actiobBtn = $('#action');
+const playboard = $('.playboard');
+// When the start btn is clicked, the playboard is displayed
+actiobBtn.on('click', (e) => {
+  e.target.style.display = 'none';
+  playboard.style.display = 'grid';
+  scrollBy(0, 1000);
+});
+
+function adjustPlayboardHeight() {
+  const headerHeight = $("header").offsetHeight;
+  playboard.style.height = `${window.innerHeight - headerHeight}px`;
+}
+
+window.onload = adjustPlayboardHeight;
+window.onresize = adjustPlayboardHeight;
